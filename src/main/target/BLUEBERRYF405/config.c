88d278ce20757cb8836dc969122ce9d9a1d320c3
Ray Morris <deepmagicbeginshere@gmail.com>
2026-03-21 01:16:59 -0500

    BLUEBERRYF405: Patch less-accessible S1 and S2

--- src/main/target/BLUEBERRYF405/config.c
+++ src/main/target/BLUEBERRYF405/config.c
@@ -20,6 +20,7 @@
 #include "fc/fc_msp_box.h"
 #include "fc/config.h"
 #include "drivers/pwm_mapping.h"
+#include "flight/mixer_profile.h"
 #include "io/piniobox.h"
 
 void targetConfiguration(void)
@@ -27,7 +28,17 @@ void targetConfiguration(void)
     pinioBoxConfigMutable()->permanentId[0] = BOX_PERMANENT_ID_USER1;
     pinioBoxConfigMutable()->permanentId[1] = BOX_PERMANENT_ID_USER2;
 
-	// S1 and S2 are not as readily accessible
+	// S1 and S2 are not as readily accessible, set default mixer to skip them
+	timerOverridesMutable(timer2id(TIM8))->outputMode = OUTPUT_MODE_SERVOS;
+	timerOverridesMutable(timer2id(TIM2))->outputMode = OUTPUT_MODE_SERVOS;
     timerOverridesMutable(timer2id(TIM1))->outputMode = OUTPUT_MODE_MOTORS;
-    timerOverridesMutable(timer2id(TIM2))->outputMode = OUTPUT_MODE_SERVOS;
+
+    *primaryMotorMixerMutable(0) = (motorMixer_t){ 1.0f, 0.0f, 0.0f, 0.0f };
+
+    *customServoMixersMutable(0) = (servoMixer_t){ .targetChannel = 1, .inputSource = 29, .rate = 100, .speed = 0 };
+    *customServoMixersMutable(1) = (servoMixer_t){ .targetChannel = 2, .inputSource = 29, .rate = 100, .speed = 0 };
+    *customServoMixersMutable(2) = (servoMixer_t){ .targetChannel = 3, .inputSource =  1, .rate = 100, .speed = 0 };
+    *customServoMixersMutable(3) = (servoMixer_t){ .targetChannel = 4, .inputSource =  0, .rate = 100, .speed = 0 };
+    *customServoMixersMutable(4) = (servoMixer_t){ .targetChannel = 5, .inputSource =  0, .rate = 100, .speed = 0 };
+    *customServoMixersMutable(5) = (servoMixer_t){ .targetChannel = 6, .inputSource =  2, .rate = 100, .speed = 0 };
 }
